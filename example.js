1d371e31e01abdd3b0f18bec708649121a51435e
Parth Mangtani <impmmmm@gmail.com>
2025-01-02 00:50:39 +0530

    📝 fix typos in example.js for access token and username/password parameters

--- example.js
+++ example.js
@@ -4,19 +4,22 @@ import {
 } from "./dist/index.js";
 
 let coreAdministrationBL = new CoreAdministrationBL();
-console.log(await coreAdministrationBL.getAllGreetingsV0("dumy_access_token"));
+console.log(await coreAdministrationBL.getAllGreetingsV0("dummy_access_token"));
 
 let authenticationAdministrationBL = new AuthenticationAdministrationBL();
 console.log(
   await authenticationAdministrationBL.registerUsernameV0(
-    "dumy_username",
-    "dumy_password",
+    "dummy_username",
+    "dummy_password",
     "dummy_password_2"
   )
 );
 console.log(
   await authenticationAdministrationBL.loginUsernameV0(
-    "dumy_username",
-    "dumy_password"
+    "dummy_username",
+    "dummy_password"
   )
+);
+console.log(
+  await authenticationAdministrationBL.removeAppForSelfV0("dummy_access_token")
 );
