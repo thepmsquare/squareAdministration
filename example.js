502cdea149964aaf6ea90fa540bd68e462e9d784
Parth Mangtani <impmmmm@gmail.com>
2025-01-01 10:29:47 +0530

    📝 add registerUsernameV0 method call to authenticationAdministrationBL in example.js

--- example.js
+++ example.js
@@ -8,6 +8,13 @@ console.log(await coreAdministrationBL.getAllGreetingsV0("dumy_access_token"));
 
 let authenticationAdministrationBL = new AuthenticationAdministrationBL();
 console.log(
+  await authenticationAdministrationBL.registerUsernameV0(
+    "dumy_username",
+    "dumy_password",
+    "dummy_password_2"
+  )
+);
+console.log(
   await authenticationAdministrationBL.loginUsernameV0(
     "dumy_username",
     "dumy_password"
